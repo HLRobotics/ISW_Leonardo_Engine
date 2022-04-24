dd2c32d5fe4687915b21b540574e8ccb2bb900f1
HLRobotics <akhil.pj007@hotmail.com>
2022-04-24 10:49:43 +0530

    main upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import Engine
-VIDEO_PATH="Relativepath of video.mp4"
+VIDEO_PATH="Coke inspection2021_08_19_12_52_5.mp4"
 FRAME="FRAMES"
 RUST="RUST"
 MARKED="MARKED"
@@ -11,9 +11,11 @@ Engine.VIdeo_to_Image(VIDEO_PATH)
 Engine.Removing_Unwanted(FRAME)
 Engine.Image_to_Rust(FRAME)
 Engine.Edge_Generator(RUST)
+Engine.Mark(EDGE)
 Engine.Image_to_Video(MARKED,RUST)
 Engine.Crack_Detection(FRAME)
 Engine.Edge_Generator(CRACK)
+Engine.Mark(EDGE)
 Engine.Image_to_Video(MARKED,CRACK)
 Engine.logger(VIDEO_PATH,False)
 print("[********COMPLETED********]")
